51e451a29c0651a18eac3f84f860826375c32eb0
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2026-02-21 01:42:53 +0100

    Admin Panel and API updated

---
+++ dtos/system.dto.ts
@@ -0,0 +1,32 @@
+export type TranscodeType = 'off' | 'compatibility' | 'smart';
+
+export interface SystemSettingsDTO {
+    features: {
+        autoTranscoding: TranscodeType;
+        concurrentProcessing: number;
+        trustEmails: boolean;
+    };
+    preferences: {
+        subtitles: { lang: string; variants: number }[];
+    };
+    external: {
+        tmdb: {
+            apiKey: string;
+        };
+        openSubtitles: {
+            apiKey: string;
+            username: string;
+            password: string;
+            useLogin: boolean;
+        };
+        email: {
+            enabled: boolean;
+            smtpSettings?: {
+                host: string;
+                port: number;
+                username: string;
+                password: string;
+            };
+        };
+    };
+}
--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export * from './dtos/pagination.dto';
 export * from './dtos/user.dto';
 export * from './dtos/notification.dto';
 export * from './dtos/socket.dto';
+export * from './dtos/system.dto';
 export * from './configs/roles';
