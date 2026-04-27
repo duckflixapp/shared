3d9fcd119f3d98735c760ec0c3e9c4abe9df14a6
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2026-04-28 01:06:28 +0200

    feat: min dto for session

--- dtos/account.dto.ts
+++ dtos/account.dto.ts
@@ -12,16 +12,20 @@ export interface AccountTwoFactorStatusDTO {
     };
 }
 
-export interface AccountSessionDTO {
+export interface AccountSessionMinDTO {
     id: string;
     deviceName: string | null;
     deviceType: 'desktop' | 'mobile' | 'tablet' | 'unknown' | null;
     browserName: string | null;
     osName: string | null;
+    lastRefreshedAt: string | null;
+    current: boolean;
+}
+
+export interface AccountSessionDTO extends AccountSessionMinDTO {
     ipAddress: string | null;
     lastIpAddress: string | null;
     lastRefreshedAt: string | null;
     expiresAt: string;
     createdAt: string;
-    current: boolean;
 }
